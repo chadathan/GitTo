5a57d485d53aac71a969aa9352f587601e6f72d3
chadathan <tonglang40@outlook.com>
2024-07-13 23:16:17 +0700

    innit

---
+++ GitTo.Server/Controllers/GitController.cs
@@ -0,0 +1,18 @@
+﻿using LibGit2Sharp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitTo.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GitController : Controller
+    {
+        [HttpGet("BranchOrigin")]
+        public void BranchOrigin(string repoPath)
+        {
+            using var repo = new Repository(repoPath);
+            var a = repo.Branches;
+            Console.WriteLine(a);
+        }
+    }
+}
